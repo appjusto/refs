6883a7a6ed3324cfecab56a1fa69fe58c6294442
Pedro Andrade <pdandradeb@gmail.com>
2022-03-11 16:04:39 -0300

    Removing storage paths from FirestoreRefs

--- FirestoreRefs.ts
+++ FirestoreRefs.ts
@@ -117,41 +117,4 @@ export class FirestoreRefs {
   getFleetsRef = () => collection(this.firestore, 'fleets');
   getFleetRef = (id: string) => doc(this.getFleetsRef(), id);
   getAppJustoFleetRef = () => this.getFleetRef('appjusto');
-
-  // storage
-  // courier
-  getCourierSelfiePath = (courierId: string, size?: string) =>
-    `couriers/${courierId}/selfie${size ? `_${size}` : ''}.jpg`;
-  getCourierDocumentPath = (courierId: string, size?: string) =>
-    `couriers/${courierId}/document${size ? `_${size}` : ''}.jpg`;
-
-  // orders
-  getOrderPODPackagePath = (orderId: string, courierId: string) =>
-    `orders/${orderId}/${courierId}/package.jpg`;
-  getOrderPODFrontPath = (orderId: string, courierId: string) =>
-    `orders/${orderId}/${courierId}/front.jpg`;
-
-  // business
-  getBusinessStoragePath = (businessId: string) => `businesses/${businessId}`;
-  getBusinessLogoUploadStoragePath = (businessId: string) =>
-    `${this.getBusinessStoragePath(businessId)}/logo.jpg`;
-  getBusinessLogoStoragePath = (businessId: string) =>
-    `${this.getBusinessStoragePath(businessId)}/logo_240x240.jpg`;
-  getBusinessCoverUploadStoragePath = (businessId: string) =>
-    `${this.getBusinessStoragePath(businessId)}/cover.jpg`;
-  getBusinessCoverStoragePath = (businessId: string) =>
-    `${this.getBusinessStoragePath(businessId)}/cover_1008x360.jpg`;
-  getProductsStoragePath = (businessId: string) =>
-    `${this.getBusinessStoragePath(businessId)}/products`;
-  getProductImageStoragePath = (
-    businessId: string,
-    productId: string,
-    size: string
-  ) => `${this.getProductsStoragePath(businessId)}/${productId}_${size}.jpg`;
-  getComplementsStoragePath = (businessId: string) =>
-    `${this.getBusinessStoragePath(businessId)}/complements`;
-  getComplementUploadStoragePath = (businessId: string, complementId: string) =>
-    `${this.getComplementsStoragePath(businessId)}/${complementId}_288x288.jpg`;
-  getComplementImageStoragePath = (businessId: string, complementId: string) =>
-    `${this.getComplementsStoragePath(businessId)}/${complementId}_288x288.jpg`;
 }
